6b8518e27279d65f05d799b41ee9ff49a108eca0
sreeragkp61 <sreeragkp61@gmail.com>
2025-09-23 03:59:13 +0530

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,9 +1,19 @@
-output "public_ip" {
-  description = "Public IP of the EC2 instance"
-  value       = aws_instance.app.public_ip
+output "instance_id" {
+  description = "ID of the EC2 instance"
+  value       = aws_instance.app_server.id
 }
 
-output "instance_id" {
-  description = "Instance ID of the EC2"
-  value       = aws_instance.app.id
+output "public_ip" {
+  description = "Public IP address of the EC2 instance"
+  value       = aws_instance.app_server.public_ip
+}
+
+output "app_url" {
+  description = "URL to access the application"
+  value       = "http://${aws_instance.app_server.public_ip}:8080"
+}
+
+output "ssh_command" {
+  description = "Command to SSH into the instance"
+  value       = "ssh -i your-key.pem ec2-user@${aws_instance.app_server.public_ip}"
 }
